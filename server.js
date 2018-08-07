4f1883bc4700ea6ad1ff925f321be967c7724e04
Glitch (jeffbot-v3) <none>
2018-08-07 09:04:36 +0000

    💴💽 Checkpoint ./server.js:1577348/12351

--- server.js
+++ server.js
@@ -1,131 +1,142 @@
 // server.js
-// where your node app starts
+// To launch server, enter 'node server.js' in terminal
 
-// init project
+/* Initialise Project Server Calls */
 const http = require('http');
 const express = require('express');
 const app = express();
 
-//global variables
-var jeffRole = ''
-
-var modRole = ''
-var modArray = []
-
-//global constants
-var prefix = ">"
-
-/* Calls */
+/* Initialise discord.js Calls */
 const Discord = require('discord.js'); //Calling discord.js Package
 const bot = new Discord.Client(); //Initialise discord bot instance
 
+//global variables
+var jeffRole = '';
+var jeffBotRole = '';
+var modRole = '';
+var modArray = [];
 
+//global constants
+const prefix = ">";
 
 /* Listener Event: Message Received */
 bot.on('message', message =>  {
-  
+
   /* Command-Argument separator */
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
   /* Variables */
-  var sender = message.author;
-  var msg = message.content.toUpperCase(); //Converts entire message to upper case
+  let msg = message.content.toUpperCase(); //Converts entire message to upper case
 
   /* Checks for botception */
   if (message.author.bot) return;
-  
 
   /* Ping Pong Function */
   if (msg === ">" + 'PING') //Checks for presence of prefix
   {
-    message.channel.send('Pongo!') //Send 'Pong' in chat channel
+    message.channel.send('Pong!') //Send 'Pong' in chat channel
   }
-  
+
   if (msg === ">" + 'START')
   {
-      message.guild.createRole({name:'Jeff'})
-      message.channel.send('It is done')
+      message.guild.createRole({name:'Jeff'});
+      message.channel.send('It is done');
       jeffRole = message.guild.roles.find("name", "Jeff")
-
+      jeffBotRole = message.guild.roles.find("name", "JeffBot")
   }
-  
-    if (msg === ">" + 'FINDJEFF')
+
+  if (msg === ">" + 'FINDJEFF')
   {
-      console.log(jeffRole.name)
-      console.log(jeffRole.id)
+      console.log(jeffRole.name);
+      console.log(jeffRole.id);
       message.channel.send(jeffRole.name)
   }
   
-    if (command === 'jefficate')
+  if (msg === ">" + 'FINDJEFFBOT')
   {
-      jeffRole = message.guild.roles.find("name", "Jeff")
-      let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-      if(!member)
-        return message.reply("Please mention a valid member of this server");
-      member.addRole(jeffRole).catch(console.error);
-
-
-      console.log(member.username,'jefficated')
-      console.log(jeffRole.id) 
+      console.log(jeffBotRole.name);
+      console.log(jeffBotRole.id);
+      message.channel.send(jeffBotRole.name)
   }
-  
-   if (command === 'unjefficate')
+
+  if (command === 'jefficate')
   {
-    if (message.member.roles.has(jeffRole.id)) return //Checks for role (Jeff), Role ID hardcoded and prevents self-unjeffication
-      console.log('Someone tried to unjefficate themeselves...')
-      jeffRole = message.guild.roles.find("name", "Jeff")
+    if (message.member.roles.has(jeffBotRole.id))
+    {
+        jeffRole = message.guild.roles.find("name", "Jeff");
+        let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+        if(!member)
+            return message.reply("Please mention a valid member of this server");
+        member.addRole(jeffRole).catch(console.error);
+
+        console.log(member.username,'jefficated');
+        console.log(jeffRole.id)
+    }
+  }
+  else
+  {
+    message.channel.send('You do not have permission to use this command')
+  }
+
+  if (command === 'unjefficate')
+  {
+    if (message.member.roles.has(jeffRole.id)) return; //Checks for role (Jeff), Role ID hardcoded and prevents self-unjeffication
+      console.log('Someone tried to unjefficate themselves...');
+      jeffRole = message.guild.roles.find("name", "Jeff");
       let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-      console.log(member)
-      if(!member)
+      console.log(member);
+      if (!member)
         return message.reply("Please mention a valid member of this server");
       member.removeRole(jeffRole).catch(console.error);
 
-      
-      console.log(member,'unjefficated')
-      console.log(jeffRole.id) 
- }
-  
-  if (command === 'setmod')
-  {
-    console.log(message.guild.members.get(args[0]))
-    modRole = message.guild.roles.find("name", message.mentions.members.first())
-    if (!modRole)
-    {
-      return message.reply("Please enter a valid role")
+      console.log(member,'unjefficated');
+      console.log(jeffRole.id)
     }
-    modArray.push(modRole)
-    console.log(modArray)
-    var modString = modArray.join(" "); //Turn array into string separated with spaces
-    message.channel.send('Current mod roles:', modString)
-  }
-  
-  /* Delete message and replace with Jeffs */
-  
-  if (!jeffRole) return
-    if (message.member.roles.has(jeffRole.id)) //Checks for role (Jeff), Role ID hardcoded
+
+    if (command === 'setmod')
     {
-      /*  Handling original user message */
-      var msgcontent = message.content
-      message.delete() //Delete the user's message from the chat channel
-      var splitStringArray = msgcontent.split(" ") //Split message into words, create array of words
-      var msgWordCount = splitStringArray.length //Check how many words in the original message
-
-      /*  For Loop to create array of 'jeff' */
-      var i; //Declare loop variable
-      var jeffArray = []; //Declare empty array
-      jeffArray.push('Jeff')
-      if (msgWordCount > 1) 
-      {
-        for (i = 0; i < msgWordCount-1; i++) { //Loop through for number of words in message
-          jeffArray.push('jeff') //Push 'jeff' to jeffArray
+        console.log(message.guild.members.get(args[0]));
+        modRole = message.guild.roles.find("name", message.mentions.members.first());
+        if (!modRole)
+        {
+            return message.reply("Please enter a valid role")
         }
-      }
+        modArray.push(modRole);
+        console.log(modArray);
+        let modString = modArray.join(" "); //Turn array into string separated with spaces
+        message.channel.send('Current mod roles:', modString)
+    }
 
-      /* Outputting jeffArray */
-      var jeffString = jeffArray.join(" "); //Turn array into string separated with spaces
-      message.channel.send(jeffString); //Send jeffString into chat channel
-      message.author.send('You just got Jeffed! Tag your friends to Jeff them also!') //PM author of original message
+    /* Delete message and replace with Jeffs */
+
+    if (!jeffRole) return;
+    if (message.member.roles.has(jeffRole.id)) //Checks for roleID of role 'Jeff' initialised on >Start
+    {
+        /*  Handling original user message */
+        message.delete(); //Delete the user's message from the chat channel
+        let splitStringArray = message.content.split(" "); //Split message into words, create array of words
+        let msgWordCount = splitStringArray.length; //Check how many words in the original message
+
+        /*  For Loop to create array of 'jeff' */
+        var i; //Declare loop variable
+        let jeffArray = []; //Declare empty array
+        jeffArray.push('Jeff');
+        if (msgWordCount > 1)
+        {
+            for (i = 0; i < msgWordCount-1; i++) { //Loop through for number of words in message
+                jeffArray.push('jeff') //Push 'jeff' to jeffArray
+            }
+        }
+
+        /* Outputting jeffArray */
+        let jeffString = jeffArray.join(" "); //Turn array into string separated with spaces
+        message.channel.send(jeffString); //Send jeffString into chat channel
+        message.author.send('You just got Jeffed! Tag your friends to Jeff them also!') //PM author of original message
+    }
+    else
+    {
+      message.channel.send('Please initialise JeffBot on your server by using ">start"');
     }
 });
 
@@ -148,11 +159,11 @@ bot.on("ready", () => {
     bot.user.setActivity(`Serving ${bot.guilds.size} Guild`); //Set Activity
   }
   */
-  
+
 });
 
 /* Login */
-bot.login(process.env.TOKEN); //Bot login with token defined in congfig.js
+bot.login(process.env.TOKEN); //Bot login with token defined in config.js
 
 
 /* Uptime pinger (Now using UptimeRobot)*/
@@ -181,4 +192,3 @@ app.get('/', function(request, response) {
 var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-
