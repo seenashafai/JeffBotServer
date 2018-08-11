76db3d7ef12f2b87fc41cbc56c30229f864d71f8
Glitch (jeffbot-v3) <none>
2018-08-11 08:33:27 +0000

    🍝🎹 Checkpoint ./server.js:1577348/4527

--- server.js
+++ server.js
@@ -106,12 +106,12 @@ bot.on('message', async message => {
                 message.channel.send('Never fear, Jeff is indeed here')
             }
             var timenow = Date.now();
-            fs.appendFile("public/log.log", timenow + " Jeff was searched for by user " + message.author.username + '(' + message.author + ') on server '+ message.guild.name + '\n'), function (err) {
+            fs.appendFile("public/log.log", timenow + " Jeff was searched for by user " + message.author.username + '(' + message.author + ') on server '+ message.guild.name + '\n', function (err) {
             if (err) {
                 return console.log(err);
             }
-          };
-        }
+          })
+        };
           
         if (msg === ">" + 'FINDJEFFINATOR') {
             jeffinatorRole = message.guild.roles.find("name", "Jeffinator");
@@ -121,107 +121,113 @@ bot.on('message', async message => {
             else {
                 message.channel.send('Never fear, Jeffinator is indeed here')
             }
-        }
-        var timenow = Date.now();
-        fs.appendFile("public/log.log", timenow + " Jeffinator was serached for by user " + message.author.username + '(' + message.author +') on server '+ message.guild.name +  '\n'), function (err) {
+            var timenow = Date.now();
+            fs.appendFile("public/log.log", timenow + " Jeffinator was serached for by user " + message.author.username + '(' + message.author +') on server '+ message.guild.name +  '\n', function (err) {
             if (err) {
-                return console.log(err);
+              return console.log(err);
             }
+          })
         };
-        if (msg === ">" + 'PURGEJEFF') {
+  
+        if (msg === ">" + 'PURGEJEFF') 
+        {
             console.log('purgejeff');
             message.channel.send('Execute order Jeffty-Jeff');
             message.guild.roles.get(jeffRole.id).delete();
             message.channel.send('It is done my lord');
             var timenow = Date.now();
-            fs.appendFile("public/log.log", timenow + " Jeff was purged by user " + message.author.username + '(' + message.author + ') on server '+ message.guild.name + '\n'), function (err) {
-                if (err) {
-                    return console.log(err);
-                }
-            };
-            if (command === 'jefficate') {
-                jeffinatorRole = message.guild.roles.find("name", "Jeffinator");
-                if (message.member.roles.has(jeffinatorRole.id)) {
-                    jeffRole = message.guild.roles.find("name", "Jeff");
-                    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-                    if (!member) {
-                        message.reply("Please mention a valid member of this server");
-                    }
-                    member.addRole(jeffRole).catch(console.error);
-                    message.channel.send(member.user.username, 'jefficated');
-                    console.log(member.user.username, 'jefficated');
-                }
-                else {
-                    message.channel.send("You do not have permission to use this command")
-                }
+            fs.appendFile("public/log.log", timenow + " Jeff was purged by user " + message.author.username + '(' + message.author + ') on server '+ message.guild.name + '\n', function (err) {
+            if (err) {
+              return console.log(err);
             }
-
-
-            if (command === 'unjefficate') {
+          })
+        };
+  
+        if (command === 'jefficate')
+        {
+            jeffinatorRole = message.guild.roles.find("name", "Jeffinator");
+            if (message.member.roles.has(jeffinatorRole.id)) {
                 jeffRole = message.guild.roles.find("name", "Jeff");
-                if (message.member.roles.has(jeffRole.id)) {
-                    //Checks for role (Jeff), Role ID hardcoded and prevents self-unjeffication
-                    console.log(message.member.user.username, 'tried to unjefficate themself...');
-                    message.channel.send(message.member.user.username, 'tried to unjefficate themself...');
-                    message.author.send("Resistance is futile")
+                let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+                if (!member) {
+                    message.reply("Please mention a valid member of this server");
                 }
-                else {
-                    jeffRole = message.guild.roles.find("name", "Jeff");
-                    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-                    if (!member)
-                        return message.reply("Please mention a valid member of this server");
-                    member.removeRole(jeffRole).catch(console.error);
-                    message.channel.send(` ${message.channel.send} unjefficated`);
-                    console.log(member.user.username, 'unjefficated');
-                }
+                member.addRole(jeffRole).catch(console.error);
+                message.channel.send(member.user.username, 'jefficated');
+                console.log(member.user.username, 'jefficated');
             }
-
-            if (command === 'jeffinate') {
-                jeffinatorRole = message.guild.roles.find("name", "Jeffinator");
-                if (message.member.roles.has(jeffinatorRole.id)) {
-                    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-                    if (!member) {
-                        message.reply("Please mention a valid member of this server");
-                    }
-                    member.addRole(jeffinatorRole).catch(console.error);
-                    console.log(message.member.username, 'was jeffinated');
-                    message.channel.send(message.member.username, "has become a Jeffinator")
-                }
-                else {
-                    message.channel.send("You must be a Jeffinator to use this command")
-                }
+            else {
+                message.channel.send("You do not have permission to use this command")
             }
-
-
-            /* Delete message and replace with Jeffs */
-
-            if (!jeffRole) return;
-            if (message.member.roles.has(jeffRole.id)) //Checks for roleID of role 'Jeff' initialised on >Start
-            {
-                /*  Handling original user message */
-                message.delete(); //Delete the user's message from the chat channel
-                let splitStringArray = message.content.split(" "); //Split message into words, create array of words
-                let msgWordCount = splitStringArray.length; //Check how many words in the original message
-
-                /*  For Loop to create array of 'jeff' */
-                var i; //Declare loop variable
-                let jeffArray = []; //Declare empty array
-                jeffArray.push('Jeff');
-                if (msgWordCount > 1) {
-                    for (i = 0; i < msgWordCount - 1; i++) { //Loop through for number of words in message
-                        jeffArray.push('jeff') //Push 'jeff' to jeffArray
-                    }
-                }
-
-                /* Outputting jeffArray */
-                let jeffString = jeffArray.join(" "); //Turn array into string separated with spaces
-                message.channel.send(jeffString); //Send jeffString into chat channel
-                message.author.send('You just got Jeffed! Tag your friends to Jeff them also!') //PM author of original message
-            }
-
         }
-    }
-);
+
+
+        if (command === 'unjefficate') 
+        {
+            jeffRole = message.guild.roles.find("name", "Jeff");
+            if (message.member.roles.has(jeffRole.id)) {
+                //Checks for role (Jeff), Role ID hardcoded and prevents self-unjeffication
+                console.log(message.member.user.username, 'tried to unjefficate themself...');
+                message.channel.send(message.member.user.username, 'tried to unjefficate themself...');
+                message.author.send("Resistance is futile")
+            }
+            else {
+                jeffRole = message.guild.roles.find("name", "Jeff");
+                let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+                if (!member)
+                    return message.reply("Please mention a valid member of this server");
+                member.removeRole(jeffRole).catch(console.error);
+                message.channel.send(` ${message.channel.send} unjefficated`);
+                console.log(member.user.username, 'unjefficated');
+            }
+        }
+
+        if (command === 'jeffinate') 
+        {
+            jeffinatorRole = message.guild.roles.find("name", "Jeffinator");
+            if (message.member.roles.has(jeffinatorRole.id)) {
+                let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+                if (!member) {
+                    message.reply("Please mention a valid member of this server");
+                }
+                member.addRole(jeffinatorRole).catch(console.error);
+                console.log(message.member.username, 'was jeffinated');
+                message.channel.send(message.member.username, "has become a Jeffinator")
+            }
+            else {
+                message.channel.send("You must be a Jeffinator to use this command")
+            }
+        }
+
+
+        /* Delete message and replace with Jeffs */
+
+        if (!jeffRole) return;
+        if (message.member.roles.has(jeffRole.id)) //Checks for roleID of role 'Jeff' initialised on >Start
+        {
+            /*  Handling original user message */
+            message.delete(); //Delete the user's message from the chat channel
+            let splitStringArray = message.content.split(" "); //Split message into words, create array of words
+            let msgWordCount = splitStringArray.length; //Check how many words in the original message
+
+            /*  For Loop to create array of 'jeff' */
+            var i; //Declare loop variable
+            let jeffArray = []; //Declare empty array
+            jeffArray.push('Jeff');
+            if (msgWordCount > 1) {
+                for (i = 0; i < msgWordCount - 1; i++) { //Loop through for number of words in message
+                    jeffArray.push('jeff') //Push 'jeff' to jeffArray
+                }
+            }
+
+            /* Outputting jeffArray */
+            let jeffString = jeffArray.join(" "); //Turn array into string separated with spaces
+            message.channel.send(jeffString); //Send jeffString into chat channel
+            message.author.send('You just got Jeffed! Tag your friends to Jeff them also!') //PM author of original message
+        }
+
+});
+
 
 
 
@@ -231,8 +237,8 @@ bot.on("ready", () => {
     //Output basic statistics of bot to console
     console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`);
     //logger.info(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`)
-    //bot.user.setActivity(`Serving ${bot.users.size} Jeffs in ${bot.guilds.size} Guilds`) //Set Activity
-    bot.user.setActivity(`Performing Maintenance - May be unresponsive`) //Set Maintenence Activity
+    bot.user.setActivity(`Serving ${bot.users.size} Jeffs in ${bot.guilds.size} Guilds`) //Set Activity
+    //bot.user.setActivity(`Performing Maintenance - May be unresponsive`) //Set Maintenence Activity
 
 });
 
